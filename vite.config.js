804dedc9f4b3f645c7adc83effb6aece20f1200b
ANJAN-23 <akarshsajlan855@gmail.com>
2025-11-07 17:51:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/'
+  base: '/portfolio/'  // This ensures GitHub Pages serves files correctly
 })
